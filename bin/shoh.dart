827d912653e5ca19f31b0f132ab957c0199ad2d5
shohrux <shohruxergashev67@gmail.com>
2022-08-07 10:57:14 +0430

    3280

---
+++ bin/shoh.dart
@@ -0,0 +1,19 @@
+int func(List<int> numbers) {
+  int y = 0;
+
+  for (int q = 0; q < numbers.length; q++) {
+    y += numbers[q];
+  }
+  /*
+Given the list of numbers, return sum of the numbers in the list
+Args:
+  numbers(List): value
+Return: 
+  Int: answer
+*/
+ return y;
+}
+
+void main() {
+  print(func([324, 3, 3, 3, 3, 3, 2, 2, 3]));
+} 
